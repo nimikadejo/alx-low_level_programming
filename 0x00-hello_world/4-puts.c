9f2cd60cbe667c928bb4dac92948c880fe108e71
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-02 15:13:04 -0700

    merging

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+/* putchar wrks like printf*/
 int main(void)
 {
-	int puts(int c,"\"Programming is like building a multilingual puzzle");
-	return 0;
+        putschar("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+/* entry point*/
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
