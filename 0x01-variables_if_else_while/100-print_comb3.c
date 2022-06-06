6cc3b3a5351ac3184ff5a089e788370fcbdf7f5e
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-06 14:53:05 -0700

    final

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,12 +14,16 @@ int main(void)
 	{
 		for (num2 = 1; num2 <= 9; num2++)
 		{
-			putchar((num1) + '0');
-			putchar((num2) + '0');
-
-			if (num1 != 8 || num2 != 9)
-			putchar(',');
-			putchar(' ');
+			if (num2 > num1)
+			{
+				putchar((num1) + '0');
+				putchar((num2) + '0');
+				if (num1 != 8 || num2 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 
