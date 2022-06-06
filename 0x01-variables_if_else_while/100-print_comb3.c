bfd5ce58b8dbbcb04d73ebed43f18b1dcec04a25
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-06 14:11:54 -0700

    advanced task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,12 @@ int main(void)
 
 	for (num1 = 0;  num1 <= 8; num1++)
 	{
-		for (num2 = 0; num2 <= 9; num2++)
+		for (num2 = 1; num2 <= 9; num2++)
 		{
 			putchar((num1) + '0');
 			putchar((num2) + '0');
-			if (num1 > 8 && num2 == 9)
-				break;
+
+			if (num1 != 8 || num2 != 9)
 			putchar(',');
 			putchar(' ');
 		}
