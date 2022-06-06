18e00818af707877fd7589c949520d140b6ece9e
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-05 21:06:25 -0700

    delete swap

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints sinlgle digits with putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		putchar((digit % 10) +'0');
+
+	putchar('\n');
+	return (0);
+}
