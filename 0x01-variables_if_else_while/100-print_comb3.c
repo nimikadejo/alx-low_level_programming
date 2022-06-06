c4d709fe23e9fc8d05f882110ca72ceaa2aa2773
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-05 20:35:11 -0700

    advanced task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints two numbers above 10 and below 99
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0;  num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 9 && num1 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
