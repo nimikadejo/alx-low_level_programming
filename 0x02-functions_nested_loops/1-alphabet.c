477f4e68fe637942510c8ca0d6290c8cac9f6f61
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-07 15:48:51 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print alphabets
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	void print_alphabet(void);
+	{
+		char alphabet = 'a';
+
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+	}
+		putchar('\n');
+		return (0);
+}
