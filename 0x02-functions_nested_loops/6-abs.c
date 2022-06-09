d24b1557958b7d83ff0576459af8993dd1898827
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-08 20:56:53 -0700

    betty corrections

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 /**
  * _abs - function prototype
  * Description: computes absolute value of an integer
- * @int: integer to be computed
+ * @num: integer to be computed
  * Return: Always 0
  */
 int _abs(int num)
