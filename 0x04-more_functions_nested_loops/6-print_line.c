5c7a811285ff8cdb5c474266fb5c08a85ada4185
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-09 21:16:04 -0700

    correct 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,23 +2,20 @@
 
 /**
  * print_line - draws straight line in terminal
- * @n - number of lines
+ * @n: number of lines
  * Return: 0 (Success)
  */
 void print_line(int n)
 {
 	int m;
 
-	if (n <= 0)
+	for (m = 0; m < n; m++)
 	{
-		_putchar('\n');
+		_putchar(95);
 	}
-	else
-	{
 		for (m = 0; m < n; m++)
 		{
 			_putchar(95);
 		}
 		_putchar('\n');
-	}
 }
