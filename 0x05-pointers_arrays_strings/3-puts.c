f6ca105a9ad75e21ee71db8767e965e8674fbb11
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-13 15:57:13 -0700

    prints string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: input string
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	int n = 0;
+
+	while (*(str + n) != '\0')
+	{
+		_putchar(*(str + n));
+		n++;
+	}
+	_putchar('\n');
+}
