0b373e1cfa7537e9df412d26dae1f96015b3124b
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-21 19:32:43 -0700

    my tasks

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,24 +8,18 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-int i;
-  int j;
+	int i;
+	int j;
 
-  while (*s != '\0' && *accept != '\0')
-    {
-      for (i = 0; *(s + i) != '\0'; i++)
-        {
-          for (j = 0; *(accept + j) != '\0'; j++)
-            {
-              if (*(s + i) == *(accept + j))
-              {
-                return (s + i);
-              }
-              else
-              {
-                return (0);
-              }
-            }
-        }
-    }
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (0);
 }
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,33 +1,28 @@
 #include "main.h"
 
 /**
- * strstr - finds the first occurence of a substring needle in the string haystack
+ * strstr - finds the first occurence of a substring needle
  * @needle: substring
  * @haystack: string
- * Return: pointer to the beginning of located substring or null
+ * Return: pointer to the located substring or null
  */
 char *_strstr(char *haystack, char *needle)
 {
-	{
-  int i;
-  int j;
+	int i;
+	int j;
 
-  for (i = 0; *haystack[i] != '\0'; i++)
-    {
-      for(j = 0; *needle[j] != '\0'; j++)
-        {
-          if(*haystack[i] != '\0' && *needle[j] != '\0')
-          {
-            if(*haystack[i] == *needle[j])
-            {
-              return (*needle)
-            }
-            else
-            {
-              return (0);
-            }
-          }
-        }
-    }
-}
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+			if (needle[j] == '\0')
+			{
+				return (&haystack[i]);
+			}
+		}
+	}
 }
