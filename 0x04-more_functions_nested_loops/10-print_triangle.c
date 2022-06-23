8d3ba7366570883b3386bfcda8a8c27e629e1ae0
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-23 14:56:27 -0700

    sqrt

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints a triangle in terminal
+ * @size: size of triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int length;
+	int width;
+
+	for (length = 0; length < size; length++)
+	{
+		for (width = 0; width < (size - i); width++);
+		forfor
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,32 @@
 #include "main.h"
 
 /**
+ * _sqrt - returns natural square root of number
+ * @n: the number
+ * @i: number
+ * Return: -1 if there isnt a natural square
+ */
+int _sqrt(int n, int i)
+{
+	int sqr = i * i;
+
+	if (sqr > n)
+	{
+		return (-1);
+	}
+	if (sqr == n)
+	{
+		return (i);
+	}
+	return (_sqrt(n, i + 1));
+}
+
+/**
  * _sqrt_recursion - returns natural square root of number
  * @n: the number
  * Return: -1 if there isnt a natural square
  */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
