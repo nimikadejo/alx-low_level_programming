eb3d6598c0b845c314c8220bc1477fdf530e8c6b
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-24 15:21:06 -0700

    argc and argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,18 @@
-#
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguements
+ * @argc: argument cunts
+ * @argv: arguments string
+ * Return: argv
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	{
+	printf("%s\n", *argv);
+	argv++;
+	}
+	return (0);
+}
