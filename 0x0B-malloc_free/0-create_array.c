4da81be3b665d5742b67b76bca2c61016f5f47c6
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-28 07:07:04 -0700

    add malloc null return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,11 +13,14 @@ char *create_array(unsigned int size, char c)
 	char *a;
 	unsigned int i = 0;
 
-	a = malloc(size * sizeof(char));
 	if (size <= 0)
 	{
 		return (0);
 	}
+	a = malloc(size * sizeof(char));
+
+	if (a == 0)
+		return (0);
 	while (i < size)
 	{
 		a[i] = c;
