e483234b2baa018409be29bb56b90d5570d6f86c
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-28 06:58:05 -0700

    add free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	char *a;
 	unsigned int i = 0;
 
-	a = malloc((size + 1) * sizeof(char));
+	a = malloc(size * sizeof(char));
 	if (size <= 0)
 	{
 		return (0);
@@ -25,4 +25,5 @@ char *create_array(unsigned int size, char c)
 	}
 	a[i] = '\0';
 	return (a);
+	free(a);
 }
