7a0a9e1dc375a968ebdc34bfe71f20fae80d51f7
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-29 12:53:03 -0700

    add for loop

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,10 +9,11 @@
  */
 int main(int argc, char *argv[])
 {
-	(void) argc;
+	int i;
+
+	for (i = 0; i > argc; i++)
 	{
-	printf("%s\n", *argv);
-	argv++;
+	printf("%s\n", argv[i]);
 	}
 	return (0);
 }
