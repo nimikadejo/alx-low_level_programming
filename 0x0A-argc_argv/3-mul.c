39b04597caf7a87bbff2bbe85ebffa658ededb3a
nimikadejo <kadejotoluwanimi@gmail.com>
2022-06-29 12:39:02 -0700

    multiplicatn

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,1 +1,29 @@
-#
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: Error or 1
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	int mul;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		mul = a * b;
+		printf("%d\n", mul);
+	}
+	return (0);
+}
