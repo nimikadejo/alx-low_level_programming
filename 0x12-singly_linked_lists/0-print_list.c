5b6d093677d53ad09e9b775cf7cde920480ffdbe
nimikadejo <kadejotoluwanimi@gmail.com>
2022-07-14 13:37:25 -0700

    add null statement

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,10 +13,10 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if ((*h).str == NULL)
-			printf("[0] (nil)");
-		else
+		if ((*h).str != NULL)
 			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)");
 		Nodes++;
 		h = h->next;
 	}
