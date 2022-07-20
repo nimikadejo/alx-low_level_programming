d6c07b160397a5dc4cf3d92b2ec18ae4a4d5c000
nimikadejo <kadejotoluwanimi@gmail.com>
2022-07-19 17:04:51 -0700

    add null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,10 +14,11 @@ void free_listint2(listint_t **head)
 	{
 		return;
 	}
-	while (*head != NULL)
+	while (*head)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
+	head = NULL;
 }
